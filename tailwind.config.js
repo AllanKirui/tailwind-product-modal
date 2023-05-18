125020ee3faa2e005c1bdb58782e29a1bcd7e84c
Allan Kirui <allan.kirui@outlook.com>
2023-05-18 16:43:07 +0300

    Add extension to tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        elgengrau: "#161719",
+        cultured: "#f1f5f9",
+        "bleu-de-france": "#3b82f6",
+        "cerulean-blue": "#1d4ed8",
+        "spanish-gray": "#999999",
+        emerald: "#4ade80",
+      },
+    },
   },
   plugins: [],
 };
